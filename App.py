4ba30d1983ee5d77c0b31417b456915e1814f72c
devaraj66 <117424874+devaraj66@users.noreply.github.com>
2024-05-11 18:22:03 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,41 +1,35 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-from PIL import Image
+from keras.preprocessing.image import img_to_array, load_img
 
 # Load your trained model
-model = tf.keras.models.load_model("model.h5")
+model = tf.keras.models.load_model('model.h5')
 
-# Define the classes for label decoding
-class_names = ['Water', 'Land', 'Road', 'Building', 'Vegetation', 'Unlabeled']
-
-# Function to preprocess and predict on user-uploaded image
+# Define function to make predictions
 def predict(image):
-    # Preprocess the image
-    image = np.array(image.resize((256, 256))) / 255.0
-    image = np.expand_dims(image, axis=0)
-    # Make prediction
-    prediction = model.predict(image)
-    predicted_class = np.argmax(prediction, axis=3)[0]
-    return predicted_class
+    img = load_img(image, target_size=(224, 224))
+    img_array = img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    prediction = model.predict(img_array)
+    return prediction
 
-# Streamlit app layout
-st.title('Semantic Segmentation with Deep Learning')
+# Streamlit app
+def main():
+    st.title('Image Segmentation App')
 
-uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+    # File uploader
+    uploaded_file = st.file_uploader("Choose an image...", type=['jpg', 'png'])
 
-if uploaded_file is not None:
-    # Display the uploaded image
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
-    st.write("Classifying...")
+    if uploaded_file is not None:
+        # Display the uploaded image
+        st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
 
-    # Make prediction
-    predicted_class = predict(image)
+        # Make prediction when 'Predict' button is clicked
+        if st.button('Predict'):
+            with st.spinner('Predicting...'):
+                prediction = predict(uploaded_file)
+                st.image(prediction, caption='Predicted Mask', use_column_width=True)
 
-    # Display the predicted segmentation mask
-    st.image(predicted_class, caption='Predicted Segmentation Mask.', use_column_width=True)
-    st.write("")
-    st.write("Class Names:")
-    st.write(class_names)
+if _name_ == '_main_':
+    main()
